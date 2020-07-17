b7b5a384fd6807fa7966ca4bb3539be7e8e07170
monisha9379 <68066248+monisha9379@users.noreply.github.com>
2020-07-17 13:58:43 +0530

    Update range.py

--- range.py
+++ range.py
@@ -4,9 +4,10 @@ for i in range(0,n):
     ele = int(input("enter elements: "))
     lst.append(ele)
 print("input list1=" ,lst)
+lst1=[]
 for j in lst:
-    if j<0:
-        lst.remove(j)
-print("output list2=" ,lst)
+    if j>0:
+        lst1.append(j)
+print("output list2=" ,lst1)
     
 
